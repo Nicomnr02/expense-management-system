c595688b7c124bcbf754e512a3c39b7b145a1f2a
Nicomnr02 <nicolasraja@synapsis.id>
2026-01-08 09:49:14 +0700

    feat(ops-migration): add password to users table

--- ops/migrations/20260107150205_create_table_users.postgres.up.sql
+++ ops/migrations/20260107150205_create_table_users.postgres.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL CHECK (role IN ('employee', 'manager')),
+    password VARCHAR NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
