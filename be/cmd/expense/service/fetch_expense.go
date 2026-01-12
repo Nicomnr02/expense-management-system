908f486f9bcb4fbd4e1d9fcd5732196bc29b4844
Nicomnr02 <nicolasraja@synapsis.id>
2026-01-12 09:26:32 +0700

    feat(be-expenses): add only-user expenses list

--- be/cmd/expense/service/fetch_expense.go
+++ be/cmd/expense/service/fetch_expense.go
@@ -4,8 +4,10 @@ import (
 	expensedto "expense-management-system/cmd/expense/dto"
 	expensequery "expense-management-system/cmd/expense/repository/query"
 	"expense-management-system/internal/contextkey"
+	middlewareenum "expense-management-system/internal/middleware/enum"
 	"expense-management-system/model"
 	"expense-management-system/pkg/currency"
+	"expense-management-system/pkg/jwt"
 
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
@@ -13,17 +15,21 @@ import (
 
 func (s *expenseServiceImpl) FetchExpense(c *fiber.Ctx, req expensedto.FetchExpenseReq) ([]expensedto.FetchExpenseRes, model.Pagination, error) {
 	var (
-		ctx  = c.Context()
-		log  = c.Locals(contextkey.Logger).(*zap.Logger)
-		data = []expensedto.FetchExpenseRes{}
-		page model.Pagination
+		ctx   = c.Context()
+		log   = c.Locals(contextkey.Logger).(*zap.Logger)
+		claim = c.Locals(contextkey.User).(*jwt.AuthClaims)
+		data  = []expensedto.FetchExpenseRes{}
+		page  model.Pagination
 	)
 
 	query := expensequery.FetchExpense{
 		ID:         req.ID,
 		Status:     req.Status,
-		UserID:     req.UserID,
 		Pagination: req.Pagination,
+	}
+
+	if claim.Role == middlewareenum.EMPLOYEEROLE {
+		query.UserID = claim.UserID
 	}
 
 	expenses, total, err := s.expenserepository.FetchExpense(ctx, query)
